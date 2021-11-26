cc9678fa60eb2acc7fcd845a8e4c2cb14ce40f1b
Dumrong <dumdumrong13@gmail.com>
2021-11-26 10:01:49 +0700

    Update v1

--- func1.php
+++ func1.php
@@ -1,3 +1,35 @@
 <?php
-//Function
+//Function PHP
+function add(){
+	$a = 10;
+	$b = 5;
+	$add = $a + $b;
+	echo "This is a Function add = ".$add."<br/>";
+
+}
+function sub(){
+	$a = 10;
+	$b = 5;
+	$sub = $a - $b;
+	echo "This is a Function Sub = ".$sub."<br/>";
+}
+function division(){
+	$a = 10;
+	$b = 5;
+	$division = $a/$b;
+	echo "This is a Function division = ".$division."<br/>";
+}
+function multiply(){
+	$a = 10;
+	$b = 5;
+	$multiply = $a * $b;
+	echo "This is a Function multiply = ".$multiply."<br/>";
+}
+
+add();
+sub();
+division();
+multiply();
+
+
 ?>
